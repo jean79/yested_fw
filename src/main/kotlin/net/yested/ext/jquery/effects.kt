cb3d999d05c3bba9d9f057da9f73271bff9da7aa
Eric Pabst <epabst@gmail.com>
2016-09-08 23:41:52 -0600

    Add some ext/jquery functionality from the old Yested.

---
+++ src/main/kotlin/net/yested/ext/jquery/effects.kt
@@ -0,0 +1,94 @@
+package net.yested.ext.jquery
+
+import jquery.JQuery
+import jquery.jq
+import net.yested.core.utils.setChild
+import org.w3c.dom.HTMLElement
+
+@native fun JQuery.fadeOut(duration:Int, callback:()->Unit) :Unit = noImpl;
+@native fun JQuery.fadeIn(duration:Int, callback:()->Unit) :Unit = noImpl;
+@native fun JQuery.slideUp(duration:Int, callback:()->Unit) :Unit = noImpl
+@native fun JQuery.slideDown(duration:Int, callback:()->Unit) :Unit = noImpl
+@native fun JQuery.show(callback:()->Unit) :Unit = noImpl;
+@native fun JQuery.hide(callback:()->Unit) :Unit = noImpl;
+
+private val DURATION = 200
+private val SLIDE_DURATION = DURATION * 2
+
+interface Effect {
+    fun apply(htmlElement: HTMLElement, callback: Function0<Unit>? = null)
+}
+
+interface BiDirectionEffect {
+    fun applyIn(htmlElement: HTMLElement, callback: Function0<Unit>? = null)
+    fun applyOut(htmlElement: HTMLElement, callback: Function0<Unit>? = null)
+}
+
+private fun call(function: Function0<Unit>?) {
+    function?.let { function() }
+}
+
+ class Show() : Effect {
+    override fun apply(htmlElement: HTMLElement, callback: (() -> Unit)?) {
+        jq(htmlElement).show { call(callback) }
+    }
+}
+
+ class Hide() : Effect {
+    override fun apply(htmlElement: HTMLElement, callback: (() -> Unit)?) {
+        jq(htmlElement).hide { call(callback) }
+    }
+}
+
+ class SlideUp() : Effect {
+    override fun apply(htmlElement: HTMLElement, callback: Function0<Unit>?) {
+        jq(htmlElement).slideUp(SLIDE_DURATION) { call(callback) }
+    }
+}
+
+ class SlideDown : Effect {
+    override fun apply(htmlElement: HTMLElement, callback: (() -> Unit)?) {
+        jq(htmlElement).slideDown(SLIDE_DURATION) { call(callback) }
+    }
+}
+
+ class FadeOut : Effect {
+    override fun apply(htmlElement: HTMLElement, callback: (() -> Unit)?) {
+        jq(htmlElement).fadeOut(DURATION) { call(callback) }
+    }
+}
+
+ class FadeIn : Effect {
+    override fun apply(htmlElement: HTMLElement, callback: (() -> Unit)?) {
+        jq(htmlElement).fadeIn(DURATION) { call(callback) }
+    }
+}
+
+ class Fade : BiDirectionEffect {
+    override fun applyIn(htmlElement: HTMLElement, callback: (() -> Unit)?) {
+        FadeIn().apply(htmlElement, callback)
+    }
+
+    override fun applyOut(htmlElement: HTMLElement, callback: (() -> Unit)?) {
+        FadeOut().apply(htmlElement, callback)
+    }
+}
+
+ class Slide : BiDirectionEffect {
+    override fun applyIn(htmlElement: HTMLElement, callback: (() -> Unit)?) {
+        SlideDown().apply(htmlElement, callback)
+    }
+
+    override fun applyOut(htmlElement: HTMLElement, callback: (() -> Unit)?) {
+        SlideUp().apply(htmlElement, callback)
+    }
+}
+
+fun HTMLElement.setChild(child: HTMLElement, effect: BiDirectionEffect, callback: Function0<Unit>? = null) {
+    effect.applyOut(this) {
+        setChild(child)
+        effect.applyIn(this) {
+            callback?.invoke()
+        }
+    }
+}
---
+++ src/main/kotlin/net/yested/ext/jquery/router.kt
@@ -0,0 +1,23 @@
+package net.yested.ext.jquery
+
+import kotlin.browser.window
+
+@native
+private interface JQRouter {
+    fun on(eventName:String, listener:() -> Unit):Unit = noImpl
+}
+
+@native("$(window)") private var routerJQuery: JQRouter = null!!
+
+private fun getHashSplitted(): Array<String> {
+    return window.location.hash.split("_").toTypedArray()
+}
+
+fun registerHashChangeListener(runNow:Boolean = true, listener:(Array<String>) -> Unit) {
+    routerJQuery.on("hashchange") {
+        listener(getHashSplitted())
+    }
+    if (runNow) {
+        listener(getHashSplitted())
+    }
+}
