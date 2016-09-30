14cd9d80fb733704d092c8c1c586715cb54a1d3b
Eric Pabst <epabst@gmail.com>
2016-09-30 14:18:31 -0600

    Add compareByValue.

--- src/main/kotlin/net/yested/core/utils/functional.kt
+++ src/main/kotlin/net/yested/core/utils/functional.kt
@@ -1,6 +1,12 @@
 package net.yested.core.utils
 
+import kotlin.comparisons.compareValues
+
 infix fun <T> T.with(doWith: T.()->Unit): T {
     this.doWith()
     return this
+}
+
+fun <T, V : Comparable<V>> compareByValue(get: (T) -> V?): (T, T) -> Int {
+   return { l, r -> compareValues(get(l), get(r)) }
 }
