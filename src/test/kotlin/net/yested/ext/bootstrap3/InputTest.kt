a875db9ef0304d20c80231d94df39af68a9c3c61
Eric Pabst <epabst@gmail.com>
2016-12-20 06:20:46 -0700

    Add test to try and diagnose why format string isn't being honored.

--- src/test/kotlin/net/yested/ext/bootstrap3/InputTest.kt
+++ src/test/kotlin/net/yested/ext/bootstrap3/InputTest.kt
@@ -29,4 +29,16 @@ class InputTest {
         moment.set(Moment.now())
         text.get().mustContain(thisYear.toString())
     }
+
+    @Test
+    fun MomentProperty_asText_shouldUseFormatString() {
+        val builder = FormatStringBuilder()
+
+        val inputFormatString = (builder.month.oneDigit + "/" + builder.dayOfMonth.oneDigit + "/" + builder.year.fourDigits).toString()
+        val moment: Property<Moment?> = Moment.parse("1/30/2015", inputFormatString).toProperty()
+
+        val formatter = builder.month.oneDigit + " @ " + builder.dayOfMonth.oneDigit + "!" + builder.year.fourDigits
+        val text: Property<String> = moment.asText(formatter.toString())
+        text.get().mustBe("1 @ 30!2015")
+    }
 }
