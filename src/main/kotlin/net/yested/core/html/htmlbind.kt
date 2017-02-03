72cf6425f2ad0398608d435cb65ed679f67c9d68
Eric Pabst <epabst@gmail.com>
2017-02-03 00:32:41 -0700

    Add HTMLSelectElement.bind.

--- src/main/kotlin/net/yested/core/html/htmlbind.kt
+++ src/main/kotlin/net/yested/core/html/htmlbind.kt
@@ -2,10 +2,10 @@ package net.yested.core.html
 
 import net.yested.core.properties.Property
 import net.yested.core.properties.ReadOnlyProperty
-import org.w3c.dom.HTMLButtonElement
-import org.w3c.dom.HTMLElement
-import org.w3c.dom.HTMLInputElement
-import org.w3c.dom.HTMLSelectElement
+import net.yested.core.properties.bind
+import net.yested.core.utils.removeAllChildElements
+import org.w3c.dom.*
+import kotlin.browser.document
 import kotlin.dom.addClass
 import kotlin.dom.removeClass
 
@@ -25,6 +25,37 @@ fun HTMLInputElement.bind(property: Property<String>) {
     addEventListener("change", { updating = true; property.set(value); updating = false }, false)
     addEventListener("keyup", { updating = true; property.set(value); updating = false }, false)
 }
+
+fun <T> HTMLSelectElement.bindMultiselect(selected: Property<List<T>>, options: Property<List<T>>, render: HTMLElement.(T)->Unit) {
+    options.onNext {
+        removeAllChildElements()
+        it.forEachIndexed { index, item ->
+            val option: HTMLOptionElement = document.createElement("option").asDynamic()
+            option.value = "$index"
+            if (selected.get().contains(item)) {
+                option.selected = true
+            }
+            option.render(item)
+            appendChild(option)
+        }
+    }
+    addEventListener("change", {
+        val selectOptions = this.options
+        val selectedValues = (1..selectOptions.length)
+                .map { selectOptions[it - 1] }
+                .filter { it.asDynamic().selected }
+                .map { it.asDynamic().value }
+                .map { options.get()[parseInt(it)] }
+
+        selected.set(selectedValues)
+    }, false)
+}
+
+fun <T> HTMLSelectElement.bind(selected: Property<T>, options: Property<List<T>>, render: HTMLElement.(T)->Unit) {
+    val multipleSelected = selected.bind({ if (it == null) emptyList() else listOf(it) }, { it.firstOrNull() as T })
+    bindMultiselect(multipleSelected, options, render)
+}
+
 
 fun HTMLElement.setClassPresence(className: String, present: ReadOnlyProperty<Boolean>) {
     present.onNext {
