79fdb385f48f99f9d3c8c796a27ee13aaa4e659d
Eric Pabst <epabst@gmail.com>
2017-03-17 14:30:32 -0600

    Add support for ajax calls.

---
+++ src/main/kotlin/net/yested/ext/jquery/ajax.kt
@@ -0,0 +1,24 @@
+package net.yested.ext.jquery
+
+data class AjaxRequest<RESULT>(val url:String, val type:String = "POST", val data:String,
+                       val contentType:String = "application/json; charset=utf-8",
+                       val dataType:String = "json", val success: ((RESULT) -> Unit))
+
+@native
+interface  JQAjax {
+    fun <T> get(url:String, loaded:(response: T) -> Unit) : Unit = noImpl
+    //fun post(url:String, data:Any?, handler:()->Unit, type:String = "json") : Unit = noImpl
+    //fun ajax(url:String, type:String, contentType:String, dataType:String, data:Any, success:()->Unit) : Unit = noImpl
+    fun <RESULT> ajax(request: AjaxRequest<RESULT>) : Unit = noImpl
+}
+
+@native("$")  var ajaxJQuery: JQAjax = null!!
+
+
+ fun <T> ajaxGet(url:String, loaded:(response:T) -> Unit) : Unit {
+    ajaxJQuery.get(url = url, loaded = loaded)
+}
+
+ fun <RESULT> ajaxPost(ajaxRequest: AjaxRequest<RESULT>) : Unit {
+    ajaxJQuery.ajax(ajaxRequest)
+}
