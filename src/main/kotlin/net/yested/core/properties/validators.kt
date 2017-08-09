6be249f86ea39d69be28704fd585e16c72268d59
Eric Pabst <epabst@gmail.com>
2017-08-08 20:24:50 -0600

    Make fun return type explicit for validator.

--- src/main/kotlin/net/yested/core/properties/validators.kt
+++ src/main/kotlin/net/yested/core/properties/validators.kt
@@ -6,14 +6,15 @@ class ValidationStatus(
     override fun toString() = if (success) "OK" else "Error: $errorMessage"
 }
 
-fun <T> ReadOnlyProperty<T>.validate(errorMessage: String, condition: (T) -> Boolean) =
-        this.map {
-            if (condition(it)) {
-                ValidationStatus(success = true, errorMessage = "")
-            } else {
-                ValidationStatus(success = false, errorMessage = errorMessage)
-            }
+fun <T> ReadOnlyProperty<T>.validate(errorMessage: String, condition: (T) -> Boolean): ReadOnlyProperty<ValidationStatus> {
+    return this.map {
+        if (condition(it)) {
+            ValidationStatus(success = true, errorMessage = "")
+        } else {
+            ValidationStatus(success = false, errorMessage = errorMessage)
         }
+    }
+}
 
 fun ReadOnlyProperty<ValidationStatus>.isValid(): Boolean {
     return get().success
